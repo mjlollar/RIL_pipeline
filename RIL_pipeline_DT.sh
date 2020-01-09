28cca7e452259ee2c8b3edde9705fd8b9a95f243
Matthew Lollar <mjlollar1@gmail.com>
2020-01-09 15:55:17 -0600

    Update RIL_pipeline_DT.sh

--- RIL_pipeline_DT.sh
+++ RIL_pipeline_DT.sh
@@ -8,24 +8,24 @@ do
 	gunzip ${RIL}_R2.fastq.gz
 
 	##### Align each read
-	bwa mem dmel_ref_r5.9.fasta ${RIL}_R1.fastq ${RIL}_R2.fastq | /mnt/sas0/AD/mlollar/samtools sort -o bam_align.bam
+	bwa mem dmel_ref_r5.9.fasta ${RIL}_R1.fastq ${RIL}_R2.fastq | /mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools sort -o bam_align.bam
 	
 	##### Remove Duplicates
-	/mnt/sas0/AD/mlollar/samtools rmdup bam_align.bam bam_rmdup.bam
+	/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools rmdup bam_align.bam bam_rmdup.bam
 	
 	##### Extract first reads in a pair
-	/mnt/sas0/AD/mlollar/samtools view -b -f 0X40 bam_rmdup.bam > bam_firsts.bam
+	/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools view -b -f 0X40 bam_rmdup.bam > bam_firsts.bam
 	
 	###### Combine all processed bams, where *BAM_FIRSTS all bams above
-	/mnt/sas0/AD/mlollar/samtools index bam_align.bam
-	/mnt/sas0/AD/mlollar/samtools index bam_rmdup.bam
-	/mnt/sas0/AD/mlollar/samtools index bam_firsts.bam
+	/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools index bam_align.bam
+	/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools index bam_rmdup.bam
+	/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools index bam_firsts.bam
 	
 	names="1 2 3"
 	
 	for name in $names
 	do
-		/mnt/sas0/AD/mlollar/samtools mpileup -q 20 -r $name bam_align.bam bam_rmdup.bam bam_firsts.bam | gzip - > ${name}.mpileup.gz
+		/mnt/sas0/AD/mlollar/bin/samtools-1.6/samtools mpileup -q 20 -r $name bam_align.bam bam_rmdup.bam bam_firsts.bam | gzip - > ${name}.mpileup.gz
 	done
 
 
@@ -60,7 +60,7 @@ do
 	##### Run AHMM
 	for name in $chroms
 	do
-		ancestry_hmm -i ${name}.ahmm_in.panel -s ahmm_in.samples -a 2 0.5 0.5 -p 0 10000 0.5 -p 1 1 0.5
+		/bin/Ancestry_HMM/src/ancestry_hmm -i ${name}.ahmm_in.panel -s ahmm_in.samples -a 2 0.5 0.5 -p 0 10000 0.5 -p 1 1 0.5
 		mv bam_rmdup.bam.posterior ../ahmm_outputs/${RIL}.${name}.bam_rmdup.bam.posterior
 		mv bam_firsts.bam.posterior ../ahmm_outputs/${RIL}.${name}.bam_firsts.bam.posterior
 		mv bam_align.bam.posterior ../ahmm_outputs/${RIL}.${name}.bam_align.bam.posterior
