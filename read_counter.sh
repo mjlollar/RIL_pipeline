e66924c0bfd90852d354dac762762236e68bfc8a
Matthew Lollar <mjlollar1@gmail.com>
2020-01-20 14:41:57 -0600

    Update read_counter.sh

--- read_counter.sh
+++ read_counter.sh
@@ -3,9 +3,9 @@
 #### Ensure you are in the directory that contains your fastq files
 #### Outputs space-delinated text file containing read count, fastq filename
 
-for file in *_R1.fastq.gz
+for file in *.fastq.gz
 do
   ##### For decompressed files use a=$(echo $(cat $file|wc -l)/4|bc)
   a=$(echo $(zcat < $file|wc -l)/4|bc)
-	echo $a $file >> R1_readcounts.txt
+	echo $a $file >> readcounts.txt
 done 
